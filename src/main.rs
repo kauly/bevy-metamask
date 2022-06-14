34761f11f983e916bf1d9635a576996c7257be1f
kauly <kauly@hotmail.com>
2022-06-14 11:06:21 -0300

    no provider

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use bevy::prelude::*;
-use bevy_egui::EguiPlugin;
+use bevy_egui::{egui, EguiContext, EguiPlugin};
 mod metamask;
 
 fn main() {
@@ -11,7 +11,45 @@ fn main() {
     app.add_plugins(DefaultPlugins)
         .add_plugin(EguiPlugin)
         .add_plugin(metamask::MetaMaskPlugin)
+        .add_system(ui_example)
         .run();
+}
+
+fn ui_example(
+    mut egui_context: ResMut<EguiContext>,
+    metamask_ch: ResMut<metamask::MetamaskChannel>,
+    app_data: Res<metamask::AppData>,
+    mut app_state: ResMut<State<metamask::AppState>>,
+) {
+    egui::CentralPanel::default().show(egui_context.ctx_mut(), |ui| {
+        let addr_tx = metamask_ch.addr_tx.clone();
+        let sign_tx = metamask_ch.sign_tx.clone();
+
+        if !app_data.no_metamask {
+            if ui.button("metamask").clicked() {
+                app_state.set(metamask::AppState::LoadingAddr).unwrap();
+                wasm_bindgen_futures::spawn_local(async move {
+                    metamask::request_account(&addr_tx).await;
+                });
+            }
+            if let Some(addr) = &app_data.user_wallet_addr {
+                let addr = addr.clone();
+                ui.label(addr.to_string());
+                if ui.button("Sign a text").clicked() {
+                    app_state.set(metamask::AppState::LoadingSign).unwrap();
+                    wasm_bindgen_futures::spawn_local(async move {
+                        metamask::sign_a_string(&sign_tx, &addr).await;
+                    })
+                }
+            }
+
+            if let Some(signed) = &app_data.signed {
+                ui.label(signed);
+            }
+        } else {
+            ui.label("no metamask");
+        }
+    });
 }
 
 #[cfg(target_arch = "wasm32")]
--- src/metamask.rs
+++ src/metamask.rs
@@ -1,7 +1,7 @@
+use std::sync::Arc;
+
 use async_channel::{bounded, Receiver, Sender};
 use bevy::prelude::*;
-use bevy_egui::{egui, EguiContext};
-use std::sync::Arc;
 use web3::transports::eip_1193;
 use web3::types::H160;
 
@@ -10,7 +10,6 @@ impl Plugin for MetaMaskPlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system(setup_comm)
             .add_state(AppState::Ready)
-            .add_system(ui_example)
             .add_system_set(
                 SystemSet::on_update(AppState::LoadingAddr).with_system(addr_response_system),
             )
@@ -20,42 +19,49 @@ impl Plugin for MetaMaskPlugin {
     }
 }
 
-struct MetamaskChannel {
+pub struct MetamaskChannel {
     addr_rx: Receiver<H160>,
-    addr_tx: Arc<Sender<H160>>,
+    pub addr_tx: Sender<H160>,
     sign_rx: Receiver<String>,
-    sign_tx: Arc<Sender<String>>,
+    pub sign_tx: Sender<String>,
 }
 
 #[derive(Clone, Eq, PartialEq, Debug, Hash)]
-enum AppState {
+pub enum AppState {
     LoadingAddr,
     LoadingSign,
     Ready,
 }
 
 #[derive(Default)]
-struct AppData {
-    user_wallet_addr: WalletAddress,
-    signed: Option<String>,
+pub struct AppData {
+    pub user_wallet_addr: Option<H160>,
+    pub signed: Option<String>,
+    pub no_metamask: bool,
 }
-
-#[derive(Default)]
-pub struct WalletAddress(Option<H160>);
 
 fn setup_comm(mut commands: Commands) {
     let (addr_tx, addr_rx) = bounded(1);
     let (sign_tx, sign_rx) = bounded(1);
     commands.insert_resource(MetamaskChannel {
         addr_rx,
-        addr_tx: Arc::new(addr_tx),
+        addr_tx,
         sign_rx,
-        sign_tx: Arc::new(sign_tx),
+        sign_tx,
     });
-    commands.insert_resource(AppData::default());
+
+    let provider = eip_1193::Provider::default().unwrap();
+    if let Some(_) = provider {
+        commands.insert_resource(AppData::default());
+    } else {
+        commands.insert_resource(AppData {
+            no_metamask: true,
+            ..AppData::default()
+        });
+    }
 }
 
-async fn request_account(addr_tx: &Sender<H160>) {
+pub async fn request_account(addr_tx: &Sender<H160>) {
     let provider = eip_1193::Provider::default().unwrap().unwrap();
     let transport = eip_1193::Eip1193::new(provider);
     let web3 = web3::Web3::new(transport);
@@ -67,7 +73,7 @@ async fn request_account(addr_tx: &Sender<H160>) {
     }
 }
 
-async fn sign_a_string(sign_tx: &Sender<String>, user_addr: &H160) {
+pub async fn sign_a_string(sign_tx: &Sender<String>, user_addr: &H160) {
     let provider = eip_1193::Provider::default().unwrap().unwrap();
     let transport = eip_1193::Eip1193::new(provider);
     let web3 = web3::Web3::new(transport);
@@ -77,40 +83,6 @@ async fn sign_a_string(sign_tx: &Sender<String>, user_addr: &H160) {
     sign_tx.send(result.to_string()).await.unwrap();
 }
 
-fn ui_example(
-    mut egui_context: ResMut<EguiContext>,
-    metamask_ch: ResMut<MetamaskChannel>,
-    app_data: Res<AppData>,
-    mut app_state: ResMut<State<AppState>>,
-) {
-    egui::CentralPanel::default().show(egui_context.ctx_mut(), |ui| {
-        let addr_tx = metamask_ch.addr_tx.clone();
-        let sign_tx = metamask_ch.sign_tx.clone();
-
-        if ui.button("metamask").clicked() {
-            app_state.set(AppState::LoadingAddr).unwrap();
-            wasm_bindgen_futures::spawn_local(async move {
-                request_account(&addr_tx).await;
-            });
-        }
-
-        if let Some(addr) = &app_data.user_wallet_addr.0 {
-            let addr = addr.clone();
-            ui.label(addr.to_string());
-            if ui.button("Sign a text").clicked() {
-                app_state.set(AppState::LoadingSign).unwrap();
-                wasm_bindgen_futures::spawn_local(async move {
-                    sign_a_string(&sign_tx, &addr).await;
-                })
-            }
-        }
-
-        if let Some(signed) = &app_data.signed {
-            ui.label(signed);
-        }
-    });
-}
-
 fn addr_response_system(
     metamask_ch: ResMut<MetamaskChannel>,
     mut app_data: ResMut<AppData>,
@@ -118,7 +90,7 @@ fn addr_response_system(
 ) {
     match metamask_ch.addr_rx.try_recv() {
         Ok(addr) => {
-            app_data.user_wallet_addr.0 = Some(addr);
+            app_data.user_wallet_addr = Some(addr);
             app_state.set(AppState::Ready).unwrap();
         }
         Err(e) => info!("{}", e),
